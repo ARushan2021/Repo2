e764076233ea4b511ce1bd8ff4f4f13e5ced1591
ARushan@appline.ru <ARushan@appline.ru>
2023-02-15 17:02:44 +0300

    Revert "нова ветка dev2"
    This reverts commit e6199bf3e8d68b4dd72bff3b9817aa333da9c97d.

--- hello.py
+++ hello.py
@@ -2,9 +2,8 @@ import bye
 
 
 name = 'Chernosliv'
-surname = 'Sortirniy slam'
-print('Hello!2')
-print('Hello World!2')
-print('особый хеллоу2', name)
+print('Hello!')
+print('Hello World!')
+print(' ', name)
 
-bye.bye_bye(name, surname)
+bye.bye_bye(name)
