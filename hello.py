e6199bf3e8d68b4dd72bff3b9817aa333da9c97d
ARushan@appline.ru <ARushan@appline.ru>
2023-02-15 17:01:43 +0300

    нова ветка dev2

--- hello.py
+++ hello.py
@@ -2,8 +2,9 @@ import bye
 
 
 name = 'Chernosliv'
-print('Hello!')
-print('Hello World!')
-print(' ', name)
+surname = 'Sortirniy slam'
+print('Hello!2')
+print('Hello World!2')
+print('особый хеллоу2', name)
 
-bye.bye_bye(name)
+bye.bye_bye(name, surname)
